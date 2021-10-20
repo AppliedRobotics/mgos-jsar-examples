ca2aeffe2c0538753c1198828926ba3026c553ba
levdm <levchenko.dmiitry@gmail.com>
2021-10-20 15:59:35 +0300

    rewrite CAN to lib

--- c/Interfaces/CAN/src/main.c
+++ c/Interfaces/CAN/src/main.c
@@ -1,7 +1,6 @@
 #include "mgos.h"
 #include "mgos_jsar.h"
-#include <ESP32CAN.h>
-#include <CAN_config.h>
+#include "mgos_jsar_can.h"
 
 CAN_device_t CAN_cfg;               // CAN Config
 const int rx_queue_size = 10;       // Receive Queue s
@@ -15,23 +14,24 @@ static void loop(void *arg)
   if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
 
     if (rx_frame.FIR.B.FF == CAN_frame_std) {
-      LOG(LL_INFO, ("New standard frame"));
-    } else {
-      LOG(LL_INFO, ("New extended frame")); 
+      mgos_uart_printf(0, "\nNew standard frame:");
+    } 
+    else {
+      mgos_uart_printf(0, "\nNew extended frame:");
     }
 
     if (rx_frame.FIR.B.RTR == CAN_RTR) {
-      LOG(LL_INFO, (" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC)); 
-    } else {
-      LOG(LL_INFO, (" from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC));
-      
+      mgos_uart_printf(0, " RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+    } 
+    else {
+      mgos_uart_printf(0, " from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+
       for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
-         LOG(LL_INFO, ("0x%02X ", rx_frame.data.u8[i]));
+         mgos_uart_printf(0, "0x%02X ", rx_frame.data.u8[i]);
       }
       
     }
   }
-
 
   /* Send CAN Message */
   CAN_frame_t tx_frame;
@@ -47,7 +47,7 @@ static void loop(void *arg)
   tx_frame.data.u8[5] = 0x3b;
   tx_frame.data.u8[6] = 0x3e;
   tx_frame.data.u8[7] = 0x3f;
-  ESP32Can.CANWriteFrame(&tx_frame);
+  mgos_jsar_can_write_frame(&tx_frame);
   
   (void) arg;
 }
@@ -62,12 +62,12 @@ enum mgos_app_init_result mgos_app_init(void)
   CAN_cfg.tx_pin_id = GPIO_NUM_2;
   CAN_cfg.rx_pin_id = GPIO_NUM_39;
   CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
-  ESP32Can.CANInit();
+  mgos_jsar_can_init();
 
   /* Timers */
   mgos_set_timer(1000 , MGOS_TIMER_REPEAT, loop, NULL);
 
-  LOG(LL_INFO, ("App init end"));
+  LOG(LL_INFO, ("Can app init end"));
 
   return MGOS_APP_INIT_SUCCESS;
 }
